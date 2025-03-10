7efe8fd95b2b021c8118b311d5f966d1a05eec35
Shiroya Avish <124157219+shiroyaavish@users.noreply.github.com>
2025-03-10 09:30:07 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+app.enableCors()
   const configService = app.get(ConfigService);
 
   const TITLE = configService.get<string>('app.title', 'Style Stamp');
